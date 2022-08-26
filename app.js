75f2d49d2095768fe5b536843622c0b95902810b
BharatKm <bharat.working057@gmail.com>
2022-08-26 19:17:41 +0530

    basic changes

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-console.log("test message")
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken")
+
+app.use(express.json())
+
+const posts = [
+  {
+    userName: "Bharat",
+    age: 28,
+  },
+  {
+    userName: "Jitesh",
+    age: 25,
+  },
+];
+
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json({ status: true, message: "this is psot endpoint1", data: posts, user: req.user });
+});
+
+app.post("/login", (req, res) => {
+    const userName = "Bharat"
+    console.log("req.body- ",req.body)
+    const accessToken = jwt.sign( userName, process.env.ACCESS_TOKEN)
+    res.json({accessToken:accessToken});
+    // res.json({"test":1112})
+});
+
+function authenticateToken( req, res, next ){
+    const authHeader = req.header('authorization')
+    const token = authHeader && authHeader.split(" ")[1]
+    if( token == null) return res.status(401).send({ success: false, message: 'Failed to authenticate token' });
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) =>{
+        if(err)
+            return res.status(403).send({ success: false, message: 'Invalid token' });
+        req.user = user
+        next()
+        
+    })
+}
+
+app.listen(4500)
